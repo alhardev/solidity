a71e05b4ac2c183ad65287191b4cf619601ab558
ALHAR ISMAIL <106420993+alhardev@users.noreply.github.com>
2023-03-07 13:24:13 +0800

    Add files via upload

---
+++ studentcount.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: GPL-3.0
+// programmer name : alhar bin ismail
+
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract studentmanagement {
+    struct StudentDataCentre {
+        uint id;
+        string name;
+        string homeAddress;
+        string course;
+        uint age;
+        bool hasGraduated;
+    }
+
+mapping(uint => StudentDataCentre) public studentData;
+
+uint public studentCount;
+
+    function setStudentDataCentre(string memory _name, string memory _homeAddress, string memory _course, uint _age, bool _hasGraduated)
+
+    public {
+
+        studentCount++;
+
+        studentData[studentCount] = StudentDataCentre(studentCount,_name,_homeAddress,_course,_age,_hasGraduated);
+    }
+
+    function getStudentId(uint _studentCount) public view returns (uint){
+
+        return (studentData[_studentCount].id);
+    }
+
+    function getStudentName(uint _studentCount) public view returns (string memory){
+
+    return (studentData[_studentCount].name);
+    }
+
+    function getStudentHomeAddress(uint _studentCount) public view returns (string memory){
+
+    return (studentData[_studentCount].homeAddress);
+    }
+
+    function getStudentCourse(uint _studentCount) public view returns (string memory){
+
+    return (studentData[_studentCount].course);
+    }
+
+    function getStudentAge(uint _studentCount) public view returns (uint){
+
+    return (studentData[_studentCount].age);
+    }
+
+    function getStudentHasGraduated(uint _studentCount) public view returns (bool){
+
+    return (studentData[_studentCount].hasGraduated);
+    }
+
+}
